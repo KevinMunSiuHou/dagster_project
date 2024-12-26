99baf8dc550aec892f1d7c238beb6e9158c9ddbe
KEVIN-LAPTOP\Kevin Mun <mshou17194@hotmail.com>
2024-12-26 23:20:21 +0800

    Update New Project

--- dagster_project/definitions.py
+++ dagster_project/definitions.py
@@ -1,9 +1,10 @@
-from dagster import Definitions, load_assets_from_modules
-
 from . import assets
+from .jobs import SCHEDULES
+from dagster import Definitions, load_assets_from_modules
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    schedules = SCHEDULES
 )
